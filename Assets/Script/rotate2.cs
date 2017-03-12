7991cbcf44a60dfe1e1094dd5274f8f60a56c570
MalvinJu <malvinjuanda@yahoo.com>
2017-03-12 10:34:36 +0700

    bike model and scene

---
+++ Assets/Script/rotate2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate2 : MonoBehaviour {
+    public float turnSpeed = 30f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.down, turnSpeed * Time.deltaTime); 
+	}
+}
